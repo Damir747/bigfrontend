617ac556a0118814b5aaceb3d12042c0777a4458
Damir <102433257+Damir747@users.noreply.github.com>
2024-02-24 10:50:27 +0300

    86. Generate Fibonacci Number

--- 86. Generate Fibonacci Number.js
+++ 86. Generate Fibonacci Number.js
@@ -3,10 +3,23 @@
  * @return {number}
  */
 function fib(n) {
-	if (n === 0 || n === 1) {
-		return 1;
+	const arr = new Array(n + 1);
+	function fibSub(n) {
+		if (n === 0) {
+			return 0;
+		}
+		if (n === 1) {
+			return 1;
+		}
+		return arr[n - 2] + arr[n - 1];
+	};
+	for (let i = 0; i <= n; i++) {
+		arr[i] = fibSub(i);
 	}
-	return fib(n - 2) + fib(n - 1);
+	return arr[n];
 }
 
+for (let i = 0; i < 1000; i++) {
+	console.log(fib(i));
+}
 
