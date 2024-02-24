1f9d90592cc612f8cf4558d64beb93525f3488bb
Damir <102433257+Damir747@users.noreply.github.com>
2024-02-24 10:57:39 +0300

    93. Generate Fibonacci Number with recursion

--- 93. Generate Fibonacci Number with recursion.js
+++ 93. Generate Fibonacci Number with recursion.js
@@ -19,15 +19,6 @@ function fib(n) {
 	return arr[n];
 }
 
-// const fib = (n) => {
-// 	if (n === 0) return 0
-// 	if (n === 1) return 1
-// 	return fib(n - 1) + fib(n - 2)
-// }
-
-// fib(10) // 55
-// fib(1000) // timeout
-
 for (let i = 0; i < 1000; i++) {
 	console.log(fib(i));
 }
