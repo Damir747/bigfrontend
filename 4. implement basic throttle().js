2d969b1e5dfc68668a92bc2673faa78161550467
Damir <102433257+Damir747@users.noreply.github.com>
2024-02-27 23:02:09 +0300

    4. implement basic throttle()

---
+++ 4. implement basic throttle().js
@@ -0,0 +1,32 @@
+/**
+ * @param {(...args:any[]) => any} func
+ * @param {number} wait
+ * @returns {(...args:any[]) => any}
+ */
+
+function throttle(func, wait) {
+	let timer;
+	let stashed;
+
+	function cooling() {
+		timer = setTimeout(check, wait);
+	};
+
+	const check = () => {
+		timer = null;
+		if (stashed) {
+			func.call(stashed[0], stashed[1]);
+			stashed = null;
+			cooling();
+		}
+	};
+
+	return function (...args) {
+		if (timer) {
+			stashed = [this, args];
+		} else {
+			func.call(this, ...args);
+			cooling();
+		}
+	}
+}
